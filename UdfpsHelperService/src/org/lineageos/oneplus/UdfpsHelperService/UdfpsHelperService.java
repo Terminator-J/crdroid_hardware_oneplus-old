b18770d6aad547bf19d3ced34d3de94ce6752281
AnierinB <anierin@evolution-x.org>
2022-05-15 18:06:33 -0700

    hardware: Introduce UdfpsHelperService
    - What's happening is that OP_DISPLAY_NOTIFY_PRESS randomly gets stuck in an on (1) mode, causing display deadlock on screen off. This service writes OP_DISPLAY_NOTIFY_PRESS 0 on every Intent.ACTION_SCREEN_OFF, thus mitigating the issue.
    
    Test W/o Service (OnePlus 6T & 8Pro):
    ~ Display on @ home screen
    ~ echo 1 > /sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/notify_fppress
    ~ Turn display off
    ~ Attempt to wake = deadlock
    ~ echo 0 > notify_fppress
    ~ Attempt to wake, device wakes
    
    Test With Service (OnePlus 6T & 8Pro):
    ~ Display on @ home screen
    ~ echo 1 > /sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/notify_fppress
    ~ Turn display off
    ~ Attempt to wake, device wakes
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ UdfpsHelperService/src/org/lineageos/oneplus/UdfpsHelperService/UdfpsHelperService.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2016 The CyanogenMod Project
+ * Copyright (C) 2022 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.oneplus.udfpshelper;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+public class UdfpsHelperService extends Service {
+    private static final String TAG = "UdfpsHelperService";
+    private static final boolean DEBUG = false;
+
+    private static final String OP_DISPLAY_NOTIFY_PRESS = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/notify_fppress";
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(OP_DISPLAY_NOTIFY_PRESS)) {
+            return OP_DISPLAY_NOTIFY_PRESS;
+        }
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+
+        IntentFilter screenStateFilter = new IntentFilter();
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        this.unregisterReceiver(mScreenStateReceiver);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                if (DEBUG) Log.d(TAG, "Display off");
+                FileUtils.writeValue(getFile(), "0");
+            }
+        }
+    };
+}

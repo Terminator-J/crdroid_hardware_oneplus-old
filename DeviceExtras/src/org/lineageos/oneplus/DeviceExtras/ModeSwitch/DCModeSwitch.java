chandranshhh/hardware_oneplus

hardware: Initial OnePlus DeviceExtras 6.2.2 import!
- Now using a better resource based configuration method to allow
  universally any OnePlus device to ship this package and configure it
  by using overlays in their device specific tree.

Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: jhenrique09 <jhsv09@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: LibXZR <xzr467706992@163.com>
Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: AnierinB <anierin@evolution-x.org>

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/ModeSwitch/DCModeSwitch.java
@@ -1,0 +1,53 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.oneplus.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.evolution.oneplus.DeviceExtras.DeviceExtras;
+
+public class DCModeSwitch implements OnPreferenceChangeListener {
+
+    private static final int NODE = R.string.node_dc_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getResources().getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(preference.getContext()), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/ModeSwitch/USB2FastChargeModeSwitch.java
@@ -1,0 +1,55 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.oneplus.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import org.evolution.oneplus.DeviceExtras.DeviceExtras;
+
+public class USB2FastChargeModeSwitch implements OnPreferenceChangeListener {
+
+    private static final int NODE = R.string.node_usb2_fast_charge_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(preference.getContext()), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/Slider/BrightnessController.java
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.oneplus.DeviceExtras.slider;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import org.evolution.oneplus.DeviceExtras.Constants;
+import org.evolution.oneplus.DeviceExtras.SliderControllerBase;
+
+public final class BrightnessController extends SliderControllerBase {
+
+    public static final int ID = 3;
+
+    private static final String TAG = "BrightnessController";
+
+    private static final int BRIGHTNESS_AUTO = 30;
+    private static final int BRIGHTNESS_BRIGHTEST = 31;
+    private static final int BRIGHTNESS_DARKEST = 32;
+
+    private static final int DARKEST = 1;
+    private static final int BRIGHTEST = 255;
+
+    public BrightnessController(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        switch (action) {
+            case BRIGHTNESS_AUTO:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC))
+                    return Constants.MODE_BRIGHTNESS_AUTO;
+                break;
+            case BRIGHTNESS_BRIGHTEST:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) &&
+                    writeSettings(Settings.System.SCREEN_BRIGHTNESS, BRIGHTEST))
+                    return Constants.MODE_BRIGHTNESS_BRIGHT;
+                break;
+            case BRIGHTNESS_DARKEST:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) &&
+                    writeSettings(Settings.System.SCREEN_BRIGHTNESS, DARKEST))
+                    return Constants.MODE_BRIGHTNESS_DARK;
+                break;
+            default:
+                return 0;
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+    }
+
+    private boolean writeSettings(String key, int value) {
+        return Settings.System.putIntForUser(mContext.getContentResolver(),
+                    key, value, UserHandle.USER_CURRENT);
+    }
+}

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/SliderControllerBase.java
@@ -1,0 +1,131 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.oneplus.DeviceExtras;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+
+import org.evolution.oneplus.DeviceExtras.Constants;
+
+public abstract class SliderControllerBase {
+
+    private static final String TAG = "SliderControllerBase";
+
+    private static final int KEY_SLIDER_TOP = 601;
+    private static final int KEY_SLIDER_MIDDLE = 602;
+    private static final int KEY_SLIDER_BOTTOM = 603;
+
+    // Vibration effects
+    private static final VibrationEffect MODE_NORMAL_EFFECT =
+            VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK);
+    private static final VibrationEffect MODE_VIBRATION_EFFECT =
+            VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK);
+
+
+    protected final Context mContext;
+    private final Vibrator mVibrator;
+
+    private int[] mActions = null;
+
+    public SliderControllerBase(Context context) {
+        mContext = context;
+        mVibrator = mContext.getSystemService(Vibrator.class);
+    }
+
+    public final void update(int[] actions) {
+        if (actions != null && actions.length == 3) {
+            mActions = actions;
+        }
+    }
+
+    public final boolean isSupported(int key) {
+        return key == KEY_SLIDER_TOP ||
+                key == KEY_SLIDER_MIDDLE ||
+                key == KEY_SLIDER_BOTTOM;
+    }
+
+    protected abstract int processAction(int action);
+
+    public final int processEvent(Context context, int key) {
+        if (mActions == null) {
+            return 0;
+        }
+
+        int result = 0;
+        switch (key) {
+            case KEY_SLIDER_BOTTOM:
+                result = processAction(mActions[0]);
+                notifySliderChange(context, result, 0);
+                break;
+            case KEY_SLIDER_MIDDLE:
+                result = processAction(mActions[1]);
+                notifySliderChange(context, result, 1);
+                doHapticFeedback(MODE_VIBRATION_EFFECT);
+                break;
+            case KEY_SLIDER_TOP:
+                result = processAction(mActions[2]);
+                notifySliderChange(context, result, 2);
+                doHapticFeedback(MODE_NORMAL_EFFECT);
+                break;
+        }
+
+        return result;
+    }
+
+    private void notifySliderChange(Context context, int result, int position) {
+        if (result > 0)
+            sendUpdateBroadcast(context, position, result);
+    }
+    
+    private void doHapticFeedback(VibrationEffect effect) {
+        if (mVibrator != null && mVibrator.hasVibrator()) {
+            mVibrator.vibrate(effect);
+        }
+    }
+
+    public static void sendUpdateBroadcast(Context context, int position, int result) {
+        Intent intent = new Intent(Constants.ACTION_UPDATE_SLIDER_POSITION);
+        intent.putExtra(Constants.EXTRA_SLIDER_POSITION, position);
+        intent.putExtra(Constants.EXTRA_SLIDER_POSITION_VALUE, result);
+        context.sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        intent.setFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY);
+        Log.d(TAG, "slider change to positon " + position);
+    }
+
+    public abstract void reset();
+
+    public final void restoreState() {
+        if (mActions == null) {
+            return;
+        }
+
+        try {
+            int state = Integer.parseInt(FileUtils.readOneLine(Constants.SLIDER_STATE));
+            processAction(mActions[state - 1]);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to restore slider state", e);
+        }
+    }
+
+    protected final <T> T getSystemService(String name) {
+        return (T) mContext.getSystemService(name);
+    }
+}

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/Touch/ResourceUtils.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.evolution.oneplus.DeviceExtras;
+
+import android.content.res.Resources;
+import android.util.Log;
+
+public class ResourceUtils {
+
+    private static final String TAG = ResourceUtils.class.getSimpleName();
+
+    public static String getLocalizedString(final Resources res,
+                                            final String stringName,
+                                            final String stringFormat) {
+        final String name = stringName.toLowerCase().replace(" ", "_");
+        final String nameRes = String.format(stringFormat, name);
+        return getStringForResourceName(res, nameRes, stringName);
+    }
+
+    public static String getStringForResourceName(final Resources res,
+                                                  final String resourceName,
+                                                  final String defaultValue) {
+        final int resId = res.getIdentifier(resourceName, "string", "org.evolution.oneplus.DeviceExtras");
+        if (resId <= 0) {
+            Log.e(TAG, "No resource found for " + resourceName);
+            return defaultValue;
+        } else {
+            return res.getString(resId);
+        }
+    }
+}

Terminator-J/crdroid_hardware_oneplus-old

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/ModeSwitch/DCModeSwitch.java
@@ -1,0 +1,54 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.oneplus.DeviceExtras.modeswitch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.lineageos.oneplus.DeviceExtras.FileUtils;
+import org.lineageos.oneplus.DeviceExtras.R;
+
+public class DCModeSwitch implements OnPreferenceChangeListener {
+
+    private static final int NODE = R.string.node_dc_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getResources().getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(preference.getContext()), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/ModeSwitch/USB2FastChargeModeSwitch.java
@@ -1,0 +1,55 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.oneplus.DeviceExtras.modeswitch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.lineageos.oneplus.DeviceExtras.FileUtils;
+import org.lineageos.oneplus.DeviceExtras.R;
+
+public class USB2FastChargeModeSwitch implements OnPreferenceChangeListener {
+
+    private static final int NODE = R.string.node_usb2_fast_charge_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(preference.getContext()), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/Slider/BrightnessController.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.oneplus.DeviceExtras.slider;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+public final class BrightnessController extends SliderControllerBase {
+
+    public static final int ID = 3;
+
+    private static final String TAG = "BrightnessController";
+
+    private static final int BRIGHTNESS_AUTO = 30;
+    private static final int BRIGHTNESS_BRIGHTEST = 31;
+    private static final int BRIGHTNESS_DARKEST = 32;
+
+    private static final int DARKEST = 1;
+    private static final int BRIGHTEST = 255;
+
+    public BrightnessController(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        switch (action) {
+            case BRIGHTNESS_AUTO:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC))
+                    return SliderConstants.MODE_BRIGHTNESS_AUTO;
+                break;
+            case BRIGHTNESS_BRIGHTEST:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) &&
+                    writeSettings(Settings.System.SCREEN_BRIGHTNESS, BRIGHTEST))
+                    return SliderConstants.MODE_BRIGHTNESS_BRIGHT;
+                break;
+            case BRIGHTNESS_DARKEST:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) &&
+                    writeSettings(Settings.System.SCREEN_BRIGHTNESS, DARKEST))
+                    return SliderConstants.MODE_BRIGHTNESS_DARK;
+                break;
+            default:
+                return 0;
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+    }
+
+    private boolean writeSettings(String key, int value) {
+        return Settings.System.putIntForUser(mContext.getContentResolver(),
+                    key, value, UserHandle.USER_CURRENT);
+    }
+}

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/Slider/SliderControllerBase.java
@@ -1,0 +1,130 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.oneplus.DeviceExtras.slider;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+
+import org.lineageos.oneplus.DeviceExtras.FileUtils;
+
+public abstract class SliderControllerBase {
+
+    private static final String TAG = "SliderControllerBase";
+
+    private static final int KEY_SLIDER_TOP = 601;
+    private static final int KEY_SLIDER_MIDDLE = 602;
+    private static final int KEY_SLIDER_BOTTOM = 603;
+
+    // Vibration effects
+    private static final VibrationEffect MODE_NORMAL_EFFECT =
+            VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK);
+    private static final VibrationEffect MODE_VIBRATION_EFFECT =
+            VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK);
+
+    protected final Context mContext;
+    private final Vibrator mVibrator;
+
+    private int[] mActions = null;
+
+    public SliderControllerBase(Context context) {
+        mContext = context;
+        mVibrator = mContext.getSystemService(Vibrator.class);
+    }
+
+    public final void update(int[] actions) {
+        if (actions != null && actions.length == 3) {
+            mActions = actions;
+        }
+    }
+
+    public final boolean isSupported(int key) {
+        return key == KEY_SLIDER_TOP ||
+                key == KEY_SLIDER_MIDDLE ||
+                key == KEY_SLIDER_BOTTOM;
+    }
+
+    protected abstract int processAction(int action);
+
+    public final int processEvent(Context context, int key) {
+        if (mActions == null) {
+            return 0;
+        }
+
+        int result = 0;
+        switch (key) {
+            case KEY_SLIDER_BOTTOM:
+                result = processAction(mActions[0]);
+                notifySliderChange(context, result, 0);
+                break;
+            case KEY_SLIDER_MIDDLE:
+                result = processAction(mActions[1]);
+                notifySliderChange(context, result, 1);
+                doHapticFeedback(MODE_VIBRATION_EFFECT);
+                break;
+            case KEY_SLIDER_TOP:
+                result = processAction(mActions[2]);
+                notifySliderChange(context, result, 2);
+                doHapticFeedback(MODE_NORMAL_EFFECT);
+                break;
+        }
+
+        return result;
+    }
+
+    private void notifySliderChange(Context context, int result, int position) {
+        if (result > 0)
+            sendUpdateBroadcast(context, position, result);
+    }
+
+    private void doHapticFeedback(VibrationEffect effect) {
+        if (mVibrator != null && mVibrator.hasVibrator()) {
+            mVibrator.vibrate(effect);
+        }
+    }
+
+    public static void sendUpdateBroadcast(Context context, int position, int result) {
+        Intent intent = new Intent(SliderConstants.ACTION_UPDATE_SLIDER_POSITION);
+        intent.putExtra(SliderConstants.EXTRA_SLIDER_POSITION, position);
+        intent.putExtra(SliderConstants.EXTRA_SLIDER_POSITION_VALUE, result);
+        context.sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        intent.setFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY);
+        Log.d(TAG, "slider change to positon " + position);
+    }
+
+    public abstract void reset();
+
+    public final void restoreState() {
+        if (mActions == null) {
+            return;
+        }
+
+        try {
+            int state = Integer.parseInt(FileUtils.readOneLine(SliderConstants.SLIDER_STATE));
+            processAction(mActions[state - 1]);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to restore slider state", e);
+        }
+    }
+
+    protected final <T> T getSystemService(String name) {
+        return (T) mContext.getSystemService(name);
+    }
+}

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/Touch/ResourceUtils.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.oneplus.DeviceExtras.touch;
+
+import android.content.res.Resources;
+import android.util.Log;
+
+public class ResourceUtils {
+
+    private static final String TAG = ResourceUtils.class.getSimpleName();
+
+    public static String getLocalizedString(final Resources res,
+                                            final String stringName,
+                                            final String stringFormat) {
+        final String name = stringName.toLowerCase().replace(" ", "_");
+        final String nameRes = String.format(stringFormat, name);
+        return getStringForResourceName(res, nameRes, stringName);
+    }
+
+    public static String getStringForResourceName(final Resources res,
+                                                  final String resourceName,
+                                                  final String defaultValue) {
+        final int resId = res.getIdentifier(resourceName, "string", "org.lineageos.oneplus.DeviceExtras");
+        if (resId <= 0) {
+            Log.e(TAG, "No resource found for " + resourceName);
+            return defaultValue;
+        } else {
+            return res.getString(resId);
+        }
+    }
+}
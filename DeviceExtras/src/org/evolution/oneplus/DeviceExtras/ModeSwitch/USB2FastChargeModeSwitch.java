chandranshhh/hardware_oneplus

hardware: Initial OnePlus DeviceExtras 6.2.2 import!
- Now using a better resource based configuration method to allow
  universally any OnePlus device to ship this package and configure it
  by using overlays in their device specific tree.

Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: jhenrique09 <jhsv09@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: LibXZR <xzr467706992@163.com>
Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: AnierinB <anierin@evolution-x.org>

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/ModeSwitch/USB2FastChargeModeSwitch.java
@@ -1,0 +1,55 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.oneplus.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import org.evolution.oneplus.DeviceExtras.DeviceExtras;
+
+public class USB2FastChargeModeSwitch implements OnPreferenceChangeListener {
+
+    private static final int NODE = R.string.node_usb2_fast_charge_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(preference.getContext()), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/Slider/RingerController.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.oneplus.DeviceExtras.slider;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.util.Log;
+import android.util.SparseIntArray;
+
+import org.evolution.oneplus.DeviceExtras.Constants;
+import org.evolution.oneplus.DeviceExtras.SliderControllerBase;
+
+public final class RingerController extends SliderControllerBase {
+
+    public static final int ID = 5;
+
+    private static final String TAG = "RingerController";
+
+    private static final int RINGER_NORMAL = 50;
+    private static final int RINGER_VIBRATE = 51;
+    private static final int RINGER_SILENT = 52;
+
+    private static final SparseIntArray MODES = new SparseIntArray();
+    static {
+        MODES.put(RINGER_NORMAL, AudioManager.RINGER_MODE_NORMAL);
+        MODES.put(RINGER_VIBRATE, AudioManager.RINGER_MODE_VIBRATE);
+        MODES.put(RINGER_SILENT, AudioManager.RINGER_MODE_SILENT);
+    }
+
+    private final AudioManager mAudioManager;
+
+    public RingerController(Context context) {
+        super(context);
+        mAudioManager = context.getSystemService(AudioManager.class);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        if (MODES.indexOfKey(action) >= 0) {
+            mAudioManager.setRingerModeInternal(MODES.get(action));
+            switch (action) {
+                case RINGER_NORMAL:
+                    return Constants.MODE_RING;
+                case RINGER_VIBRATE:
+                    return Constants.MODE_VIBRATE;
+                case RINGER_SILENT:
+                    return Constants.MODE_SILENT;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+    }
+}

Terminator-J/crdroid_hardware_oneplus-old

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/ModeSwitch/USB2FastChargeModeSwitch.java
@@ -1,0 +1,55 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.oneplus.DeviceExtras.modeswitch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.evolution.oneplus.DeviceExtras.FileUtils;
+import org.evolution.oneplus.DeviceExtras.R;
+
+public class USB2FastChargeModeSwitch implements OnPreferenceChangeListener {
+
+    private static final int NODE = R.string.node_usb2_fast_charge_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(preference.getContext()), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/evolution/oneplus/DeviceExtras/Slider/RingerController.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.oneplus.DeviceExtras.slider;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.util.Log;
+import android.util.SparseIntArray;
+
+public final class RingerController extends SliderControllerBase {
+
+    public static final int ID = 5;
+
+    private static final String TAG = "RingerController";
+
+    private static final int RINGER_NORMAL = 50;
+    private static final int RINGER_VIBRATE = 51;
+    private static final int RINGER_SILENT = 52;
+
+    private static final SparseIntArray MODES = new SparseIntArray();
+    static {
+        MODES.put(RINGER_NORMAL, AudioManager.RINGER_MODE_NORMAL);
+        MODES.put(RINGER_VIBRATE, AudioManager.RINGER_MODE_VIBRATE);
+        MODES.put(RINGER_SILENT, AudioManager.RINGER_MODE_SILENT);
+    }
+
+    private final AudioManager mAudioManager;
+
+    public RingerController(Context context) {
+        super(context);
+        mAudioManager = context.getSystemService(AudioManager.class);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        if (MODES.indexOfKey(action) >= 0) {
+            mAudioManager.setRingerModeInternal(MODES.get(action));
+            switch (action) {
+                case RINGER_NORMAL:
+                    return SliderConstants.MODE_RING;
+                case RINGER_VIBRATE:
+                    return SliderConstants.MODE_VIBRATE;
+                case RINGER_SILENT:
+                    return SliderConstants.MODE_SILENT;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+    }
+}